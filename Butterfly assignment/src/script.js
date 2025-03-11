30b4f2827b6e07973cace5381c127a66b5c3ca68
K1651897 <45717736+K1651897@users.noreply.github.com>
2025-03-11 19:03:34 +0000

    Add files via upload
    Butterly assignment

---
+++ Butterfly assignment/src/script.js
@@ -0,0 +1,11 @@
+function increaseTextSize() {
+    document.body.style.fontSize = '120%';
+}
+
+function decreaseTextSize() {
+    document.body.style.fontSize = '100%';
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+}
